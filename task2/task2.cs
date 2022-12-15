3a47e2e15a00db6a60104fc32d0046da19c5e201
Danis Sharipov <danshadance@mail.ru>
2022-12-15 22:33:25 +0600

    пробежался еще раз по лекции

--- task2/task2.cs
+++ task2/task2.cs
@@ -1,14 +1,23 @@
 ﻿double Factorial (int n)
 {
-    // 1! = 1
-    // 0! = 1
     if (n == 1) return 1;
     else return n * Factorial(n - 1);
 }
 
-Console.WriteLine(Factorial(40)); // 1*2*3 = 6
+Console.WriteLine(Factorial(40));
 
 for ( int i = 1; i < 40; i++)
 {
     Console.WriteLine($"{i} = {Factorial(i)}");
 }
+
+Console.WriteLine();
+
+int Sum (int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return n + Sum(n-2);
+}
+
+Console.WriteLine(Sum(3));
